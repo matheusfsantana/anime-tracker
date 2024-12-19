fa5b4f153ca080b51bd3a73c0a83a29417e12e58
WillianDDaniel <williandeivitidaniel@live.com>
2024-12-19 19:05:35 -0300

    added home page

--- spec/system/user_see_homepage_spec.rb
+++ spec/system/user_see_homepage_spec.rb
@@ -3,6 +3,12 @@ require 'rails_helper'
 describe 'User sees homepage' do
   it 'sees homepage' do
     visit root_path
-    expect(page).to have_content('Bem vindo')
+
+    expect(page).to have_content('AnimeTracker')
+
+    within('nav') do
+      expect(page).to have_link('Login')
+      expect(page).to have_link('Criar Conta')
+    end
   end
 end
