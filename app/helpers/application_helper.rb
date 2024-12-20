5f4e79c74ada0f5d52e96e631a0cc4fee81c5980
fabio-k <fabiokmain@gmail.com>
2024-12-20 11:06:46 -0300

    refactor error handling on form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def form_error(model, param)
+    if model.errors[param].any?
+      content_tag(:span, class: "text-red-500 text-sm") do
+        model.errors[param].first
+      end
+    end
+  end
 end
